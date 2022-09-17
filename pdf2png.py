232bd57895549018f92e01b3b6ecdd5c290bb853
Blue Adam <32387513+nineship@users.noreply.github.com>
2022-09-17 12:32:14 +0800

    Update and rename pdf2png to pdf2png.py

--- pdf2png.py
+++ pdf2png.py
@@ -14,8 +14,8 @@ def pyMuPDF_fitz(pdfPath, imagePath):
         rotate = int(0)
         # 每个尺寸的缩放系数为1.3，这将为我们生成分辨率提高2.6的图像。
         # 此处若是不做设置，默认图片大小为：792X612, dpi=96
-        zoom_x = 2  # (1.33333333-->1056x816)   (2-->1584x1224)
-        zoom_y = 2
+        zoom_x = 3.137  # (1.33333333-->1056x816)   (2-->1584x1224)
+        zoom_y = 3.137
         mat = fitz.Matrix(zoom_x, zoom_y).preRotate(rotate)
         pix = page.getPixmap(matrix=mat, alpha=False)
 
