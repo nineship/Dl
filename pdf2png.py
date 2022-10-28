77751a6afedcf6eab14180204a560e6d26efa8d1
Blue Adam <32387513+nineship@users.noreply.github.com>
2022-10-28 22:07:50 +0800

    Update pdf2png.py

--- pdf2png.py
+++ pdf2png.py
@@ -26,16 +26,20 @@ def pyMuPDF_fitz(pdfPath, imagePath):
 
     endTime_pdf2img = datetime.datetime.now()  # 结束时间
     print('pdf2img时间=', (endTime_pdf2img - startTime_pdf2img).seconds)
+    text = str(pdfDoc.page_count)
+    saveFile = open('./calc.txt', 'w')
+    saveFile.write(text)
+    saveFile.close()
 def c():
     num = os.listdir("./imgs/") # num为list类型，存储着每张图片的名字
     print('共{}张图片'.format(len(num))) # 计算机当前目录有几张图片
     ori_path = "./imgs/"
     save_path = "./save/"
     for i in range(len(num)):
-        img = cv2.imread(ori_path+num[i])  # 读取
-        print(i)
-        resize = cv2.resize(img, (1920, 2716))  # resize
-        cv2.imwrite(save_path+str(i+1)+'.bmp',resize)  # 保存
+        img = cv2.imread(ori_path+str(i+1)+".png")  # 读取
+        resize = cv2.resize(img, (1200, 1600))  # resize
+        #resize = img
+        cv2.imwrite(save_path+str(i+1)+'.jpg',resize)  # 保存
 
 
 if __name__ == "__main__":
@@ -43,5 +47,7 @@ if __name__ == "__main__":
     pdfPath = 'test.pdf'
     # 2、需要储存图片的目录
     imagePath = './imgs'
-    pyMuPDF_fitz(pdfPath, imagePath)
-    #c()
+    #pyMuPDF_fitz(pdfPath, imagePath)
+    c()
+    
+
