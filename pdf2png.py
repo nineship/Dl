7dc2e14d368db0565206d747f817ec460f76ff74
Blue Adam <32387513+nineship@users.noreply.github.com>
2022-10-28 20:27:27 +0800

    Update pdf2png.py

--- pdf2png.py
+++ pdf2png.py
@@ -9,20 +9,20 @@ def pyMuPDF_fitz(pdfPath, imagePath):
 
     print("imagePath=" + imagePath)
     pdfDoc = fitz.open(pdfPath)
-    for pg in range(pdfDoc.pageCount):
+    for pg in range(pdfDoc.page_count):
         page = pdfDoc[pg]
         rotate = int(0)
         # 每个尺寸的缩放系数为1.3，这将为我们生成分辨率提高2.6的图像。
         # 此处若是不做设置，默认图片大小为：792X612, dpi=96
         zoom_x = 3.137  # (1.33333333-->1056x816)   (2-->1584x1224)
         zoom_y = 3.137
-        mat = fitz.Matrix(zoom_x, zoom_y).preRotate(rotate)
-        pix = page.getPixmap(matrix=mat, alpha=False)
+        mat = fitz.Matrix(zoom_x, zoom_y).prerotate(rotate)
+        pix = page.get_pixmap(matrix=mat, alpha=False)
 
         if not os.path.exists(imagePath):  # 判断存放图片的文件夹是否存在
             os.makedirs(imagePath)  # 若图片文件夹不存在就创建
 
-        pix.writePNG(imagePath + '/' + '%s.png' % (pg+1))  # 将图片写入指定的文件夹内
+        pix.save((imagePath + '/' + str(pg+1)+'.png'))  # 将图片写入指定的文件夹内
 
     endTime_pdf2img = datetime.datetime.now()  # 结束时间
     print('pdf2img时间=', (endTime_pdf2img - startTime_pdf2img).seconds)
@@ -44,4 +44,4 @@ if __name__ == "__main__":
     # 2、需要储存图片的目录
     imagePath = './imgs'
     pyMuPDF_fitz(pdfPath, imagePath)
-    c()
+    #c()
